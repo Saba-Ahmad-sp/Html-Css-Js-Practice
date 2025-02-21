2584de590b4d61e531f208d6a05f5b43b06e1c1e
Saba Ahmad <sabaahmad.sp@gmail.com>
2025-02-21 17:28:43 +0530

    fixed some code for js logic and css

--- Analog-clock/script.js
+++ Analog-clock/script.js
@@ -1,20 +1,22 @@
-const hr = document.getElementById("hour");
-const min = document.getElementById("minute");
-const sec = document.getElementById("second");
+let hr = document.getElementById("hour");
+let min = document.getElementById("minute");
+let sec = document.getElementById("second");
 
 function displayTime(){
-const date = new Date();
+  let date = new Date();
 
-const hh = date.getHours();
-const mm = date.getMinutes();
-const ss = date.getSeconds();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
 
-const hRotation = 30*hh + mm/2;
-const mRotation = 6*mm;
-const sRotation = 6*ss;
+  let hRotation = 30*hh + mm/2;
+  let mRotation = 6*mm;
+  let sRotation = 6*ss;
 
-hr.style.transform = `rotate(${hRotation}deg)`;
-min.style.transform = `rotate(${mRotation}deg)`;
-sec.style.transform = `rotate(${sRotation}deg)`;
+  hr.style.transform = `rotate(${hRotation}deg)`;
+  min.style.transform = `rotate(${mRotation}deg)`;
+  sec.style.transform = `rotate(${sRotation}deg)`;
 }
+
 setInterval(displayTime, 1000);
+
